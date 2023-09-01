0b7f705cb56ac90eef9f9c22cef43d09e1aa9454
butter-cloud <130043415+butter-cloud@users.noreply.github.com>
2023-09-02 00:11:55 +0900

    Create background.js

---
+++ js/background.js
@@ -0,0 +1,16 @@
+const images = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg"
+]
+
+const chosenimage = images[Math.floor(Math.random()*images.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `./img/${chosenimage}`;
+
+document.body.appendChild(bgImage);
